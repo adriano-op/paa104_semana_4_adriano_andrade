5eaa60bf7945fd9f8824e6c67ce926b9d3a2a3f1
05701265684 <adriano.andrade@ufop.edu.br>
2021-05-04 18:12:31 -0300

    att lista de exerci

--- main.cpp
+++ main.cpp
@@ -2,26 +2,176 @@
 #include <vector>
 #include <iterator>
 #include <cmath>
+#include <random>
 
 using namespace std;
+
+/* C implementation QuickSort */
+#include<stdio.h>
+
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+/* Esta função pega o último elemento como pivô, coloca
+    o elemento pivô em sua posição correta na classificação
+     array, e coloca todos menores (menores que pivô)
+    à esquerda do pivô e todos os elementos maiores à direita
+    de pivô */
+template<class T>
+int partition (std::vector<T> &arr, int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/*
+ * quicksort ou ordenação por troca de partições
+-ideia básica: dividir e conquistar.
+-um elemento é escolhido como pivo.
+-"particionar": os dados são rearranjados (valores menores do que o pivô são colocados antes dele e os maiores, depois).
+-Recursivamente ordena as 2 partições.
+
+"performance"
+- melhor caso: O(n log n)
+-Pior caso(raro): O(n²)
+-Estável: não altera a ordem de dados iguais
+-Desvantagens: como escolher o pivô?
+*/
+template<class T>
+void quickSort(std::vector<T> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        /* pi é o índice de particionamento, arr[p] agora é    no lugar certo */
+        int pi = partition(arr, low, high);
+
+        // Classifique os elementos separadamente antes
+        // partição e depois da partição
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+
+void printArray(int A[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << A[i] << " ";
+    cout << "\n";
+}
+
+template<class T>
+int posicaoMaioreMenorElemento(std::vector<T> &arr, int begin, int end, int max, int min) {
+    int max1  , min1 ;
+    if (begin == end) {
+        min = arr[begin];
+        max = arr[begin];
+        max1 =arr[begin];
+        min1 =arr[begin];
+    }
+    else if( (begin == end - 1)) {
+        if(arr[begin] < arr[end] ){
+            max=arr[end];
+            min=arr[begin];
+        } else{
+            max=arr[begin];
+            min=arr[end];
+        }
+    } else{
+        int m= (begin + end)/2;
+        posicaoMaioreMenorElemento(arr,begin,m, max1,min1);
+        posicaoMaioreMenorElemento(arr,m+1,end, max,min);
+        if(max1 < max){
+            max=max1;
+        }
+        if(min1 > min){
+            min=min1;
+        }
+        cout << "Max: " << max << " Min: " << min << endl;
+    }
+
+//    int max1, min1;
+//    if (begin == end) {
+//        min = arr[begin];
+//        max = arr[begin];
+//        max1 = arr[begin];
+//        min1 = arr[begin];
+//    } else if ((end - begin) == 1) {
+//        if (arr[begin] <= arr[end]) {
+//            min = arr[end];
+//            max = arr[end];
+//        } else {
+//            min = arr[end];
+//            max = arr[begin];
+//        }
+//    } else if ((end - begin) > 1) {
+//        int m = (begin + end) / 2;
+//        posicaoMaioreMenorElemento(arr, 0, m, max, min);
+//        posicaoMaioreMenorElemento(arr, m + 1, end, max1, min1);
+//        if (max1 > max) {
+//            max = max1;
+//        }
+//        if (min1 < min) {
+//            min = min1;
+//        }
+//        cout << "max: " << max << "   Min " << min << endl;
+//    }
+}
+
+template<class T>
+int posicaoMaiorElemento(std::vector<T> &arr, int begin, int end) {
+    if (begin == end) {
+        return begin;
+    }
+    int m = ((begin + end) / 2);
+
+    int p1 = posicaoMaiorElemento(arr, begin, m);        //2T(n/2) -> divide em subproblemas
+    int p2 = posicaoMaiorElemento(arr, m + 1, end); //2T(n/2)
+
+    if (arr[p1] >= arr[p2]) {
+//            cout << "arr[p1] " << arr[p1]  << endl;
+        return p1;
+    } else {
+        //          cout << "arr[p2] " << arr[p2]  << endl;
+        return p2;
+    }
+
+}
 
 
 // Mescla dois subarrays de arr [].
 // O primeiro subarray é arr [l..m]
 // O segundo subarray é arr [m + 1..r]
-void merge(std::vector<int> &arr, int l, int m, int r) {
-    int n1 = m - l + 1; // primeira metade
-    int n2 = r - m;     // segunda metade
+template<class T>
+void merge(std::vector<T> &arr, int l, int m, int r) {
+    int x = m - l + 1; // primeira metade
+    int y = r - m;     // segunda metade
 
     // Create temp arrays
-    std::vector<int> L(n1), R(n2);
+    std::vector<T> B(x), C(y);
 
-// Copiar dados para matrizes temporárias L [] e R []
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (int j = 0; j < n2; j++)
-        //moedaExtra.push_back(coins[i]);
-        R[j] = arr[m + 1 + j];
+// Copiar dados para matrizes temporárias B [] e C []
+    for (int i = 0; i < x; i++)
+        B[i] = arr[l + i];
+    for (int j = 0; j < y; j++)
+        C[j] = arr[m + 1 + j];
 
     // Mesclar as matrizes temporárias de volta em arr [l..r]
 
@@ -34,43 +184,42 @@ void merge(std::vector<int> &arr, int l, int m, int r) {
     // Índice inicial do subarray mesclado
     int k = l;
 
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
+    while (i < x && j < y) {
+        if (B[i] <= C[j]) {
+            arr[k] = B[i];
             i++;
         } else {
-            arr[k] = R[j];
+            arr[k] = C[j];
             j++;
         }
         k++;
     }
 
-// Copie os elementos restantes de
-    // L [], se houver algum
-    while (i < n1) {
-        arr[k] = L[i];
+// Copie os elementos restantes de  B[], se houver algum
+    while (i < x) {
+        arr[k] = B[i];
         i++;
         k++;
     }
 
-    // Copy the remaining elements of
-    // R[], if there are any
-    while (j < n2) {
-        arr[k] = R[j];
+    // Copie os elementos restantes de  C[], se houver algum
+    while (j < y) {
+        arr[k] = C[j];
         j++;
         k++;
     }
 }
 
-void mergeSort(std::vector<int> &arr, int l, int r) {
-    if (l > = r) {
+template<class T>
+void mergeSort(std::vector<T> &arr, int l, int r) {
+    if (l >= r) {
         return;
     }
-    int m = (l +r )/ 2;
+    int m = (l + r) / 2;
 
-    mergeSort(arr, l, m);        //2T(n/2)
+    mergeSort(arr, l, m);        //2T(n/2) -> divide em subproblemas
     mergeSort(arr, m + 1, r); //2T(n/2)
-    merge(arr, l, m, r); // O(n)
+    merge(arr, l, m, r); // O(n) -> intercala os arrays
 }
 
 
@@ -83,10 +232,7 @@ void printVector(std::vector<T> &v) {
 }
 
 
-void printArray(int A[], int size) {
-    for (int i = 0; i < size; i++)
-        cout << A[i] << " ";
-}
+
 
 std::vector<int> inicializaVector(int n) {
     std::vector<int> v;
@@ -115,6 +261,89 @@ int main() {
     //----------------------------------------- MergeSort --------------------------------------------------------------
     //------------------------------------------------------------------------------------------------------------------
 
+//    int n = 20;
+//    cout << "Tamanho do Vetor: " << n << endl;
+//    std::vector<int> arr = inicializaVectorRandom(n); // O(n)
+//
+//    cout << "O array: \n";
+//    printVector(arr);
+//
+//
+//    mergeSort(arr, 0, arr.size() - 1);
+//
+//    cout << "Array ordenado: \n";
+//    printVector(arr);
+
+    ///22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+    //----------------------------------------- MergeSort ----- String--------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+
+//    std::vector<string> arr = {"E", "X", "A","M", "P", "L", "E"};//inicializaVectorRandom(n); // O(n)
+//    cout << "Tamanho do Vetor: " <<arr.size() << endl;
+//
+//    cout << "O array: \n";
+//    printVector(arr);
+//    mergeSort(arr, 0, arr.size() - 1);
+//
+//    cout << "Array ordenado: \n";
+//    printVector(arr);
+
+
+    ///33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+    //----------------------------------------- resposta no  pdf --------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+
+
+
+    ///44444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    //----------------------------------------- posicaoMaiorElemento  --------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+
+//    int n = 10;
+//
+//    std::vector<int> arr = inicializaVectorRandom(n); // O(n)
+//
+//    arr.push_back(984);
+//    arr.push_back(984);
+//    arr.push_back(984);
+//
+//    cout << "Tamanho do Vetor: " << arr.size() << endl;
+//    cout << "O array: " << endl;
+//
+//    printVector(arr);
+//
+//
+//    int p = posicaoMaiorElemento(arr, 0, arr.size() - 1);
+//    cout << "arr[p1] " << arr[p] << " na posição: " << p << endl;
+
+
+    ///55555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
+    //----------------------------------------- posicaoMaioreMenorElemento  --------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+
+
+//    int n = 10;
+//
+//    std::vector<int> arr = {97, 494, 564, 909, 542, 124, 8, 605, 540, 884};//inicializaVectorRandom(n); // O(n)
+//
+////    arr.push_back(984);
+////    arr.push_back(984);
+////    arr.push_back(984);
+//
+//    cout << "Tamanho do Vetor: " << arr.size() << endl;
+//    cout << "O array: " << endl;
+//
+//    printVector(arr);
+//
+//
+//    posicaoMaioreMenorElemento(arr, 0, arr.size() , 0, 0);
+
+
+    //666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666
+    //----------------------------------------------- quickSort  -------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+
+
     int n = 20;
     cout << "Tamanho do Vetor: " << n << endl;
     std::vector<int> arr = inicializaVectorRandom(n); // O(n)
@@ -123,9 +352,11 @@ int main() {
     printVector(arr);
 
 
-    mergeSort(arr, 0, arr.size() - 1);
+    quickSort(arr, 0, arr.size() - 1);
 
     cout << "Array ordenado: \n";
     printVector(arr);
+
+
     return 0;
 }
