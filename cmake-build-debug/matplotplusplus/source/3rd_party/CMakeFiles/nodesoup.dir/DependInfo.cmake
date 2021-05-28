084923951cb78e051b4869d289f8d12b77d1768b
05701265684 <adriano.andrade@ufop.edu.br>
2021-05-28 19:58:05 -0300

    inclusao do strassen DC

---
+++ cmake-build-debug/matplotplusplus/source/3rd_party/CMakeFiles/nodesoup.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/3rd_party/nodesoup/src/algebra.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/3rd_party/CMakeFiles/nodesoup.dir/nodesoup/src/algebra.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/3rd_party/nodesoup/src/fruchterman_reingold.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/3rd_party/CMakeFiles/nodesoup.dir/nodesoup/src/fruchterman_reingold.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/3rd_party/nodesoup/src/kamada_kawai.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/3rd_party/CMakeFiles/nodesoup.dir/nodesoup/src/kamada_kawai.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/3rd_party/nodesoup/src/layout.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/3rd_party/CMakeFiles/nodesoup.dir/nodesoup/src/layout.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/3rd_party/nodesoup/src/nodesoup.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/3rd_party/CMakeFiles/nodesoup.dir/nodesoup/src/nodesoup.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_USE_MATH_DEFINES"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../matplotplusplus/source/3rd_party/nodesoup/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/bars.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/bars.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/box_chart.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/box_chart.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/circles.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/circles.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/contours.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/contours.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/error_bar.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/error_bar.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/filled_area.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/filled_area.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/function_line.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/function_line.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/histogram.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/histogram.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/labels.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/labels.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/line.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/line.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/matrix.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/matrix.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/network.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/network.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/parallel_lines.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/parallel_lines.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/stair.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/stair.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/string_function.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/string_function.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/surface.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/surface.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/axes_objects/vectors.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/vectors.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/backend/backend_interface.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/backend/backend_interface.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/backend/backend_registry.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/backend/backend_registry.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/backend/gnuplot.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/backend/gnuplot.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/core/axes_object.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/axes_object.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/core/axes_type.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/axes_type.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/core/axis_type.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/axis_type.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/core/figure_registry.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/figure_registry.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/core/figure_type.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/figure_type.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/core/legend.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/legend.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/core/line_spec.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/line_spec.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/freestanding/axes_functions.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/freestanding/axes_functions.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/freestanding/histcounts.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/freestanding/histcounts.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/util/colors.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/colors.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/util/common.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/common.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/util/contourc.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/contourc.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/util/world_cities.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_cities.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/util/world_map_10m.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_map_10m.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/util/world_map_110m.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_map_110m.cpp.o"
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/matplotplusplus/source/matplot/util/world_map_50m.cpp" "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_map_50m.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CXX_FILESYSTEM_IS_EXPERIMENTAL"
+  "MATPLOT_BUILD_HIGH_RESOLUTION_WORLD_MAP"
+  "MATPLOT_HAS_FBUFSIZE"
+  "cimg_display=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../matplotplusplus/source"
+  "../matplotplusplus/source/3rd_party/cimg"
+  "../matplotplusplus/source/3rd_party/nodesoup/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/media/adriano/DATA/Google Drive/Mestrado/UFOP/2020/PCC104/lista/semana4/cmake-build-debug/matplotplusplus/source/3rd_party/CMakeFiles/nodesoup.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
